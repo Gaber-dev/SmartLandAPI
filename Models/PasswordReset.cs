6512392b6eb440b07dd0f8e42d8fb05229385b6b
Gaber-eg <133911723+Gaber-dev@users.noreply.github.com>
2025-09-14 00:18:25 +0300

    Update PasswordReset.cs

--- Models/PasswordReset.cs
+++ Models/PasswordReset.cs
@@ -4,9 +4,10 @@
     {
         public int Id { get; set; }
         public int UserId { get; set; }
-        public string Code { get; set; } = string.Empty; // 4-digit code
+        public string Code { get; set; } = string.Empty; 
         public DateTime Expiry { get; set; }
         public bool IsUsed { get; set; }
         public User? User { get; set; }
     }
 }
+
